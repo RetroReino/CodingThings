5f220144f02d142a1fcdd702e86a93c84fe50f28
RetroReino <ReinoBecker@icloud.com>
2025-01-14 18:33:23 -0500

    track works, some minor changes

--- Time.java
+++ Time.java
@@ -2,23 +2,30 @@ import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class Time {
+    private static TimeUnit start = TimeUnit.SECONDS;
     private static Scanner in = new Scanner(System.in);
     public static void main( String[] args ){
-        TimeUnit start = TimeUnit.SECONDS;
         System.out.println( "This is the time class." );
+        track( start );
+
+        System.out.println( "current system time is " + start );
     }
-    public void track( TimeUnit time ){
+    public static void track( TimeUnit time ){
         long startTime = System.currentTimeMillis();
         System.out.println( "press a button to continue" );
-        String answer = in.nextLine();
-        if (answer != null) {
-            long endTime = System.currentTimeMillis();
-            int timeInSec = (int)( endTime - startTime ) / 1000;
-            System.out.println( "You have waited" + timeInSec + "seconds." );
-        }
-        System.out.println( "current system time is " + time );
+        String answer = in.nextLine() + "a";
+        long endTime = System.currentTimeMillis();
+        int timeInSec = (int)( endTime - startTime ) / 1000;
+        System.out.println( "You have waited " + timeInSec + " " + start );
     }
-    public void pause(){
+    public static void pause(){
         
+    }
+    public void restart(){
+        System.out.println( "would you like to restart the timer? Y/N" );
+        String answer = System.console().readLine();
+        if( answer.equals("Y") || answer.equals("y") ){
+            start = TimeUnit.SECONDS;
+        }
     }
 }
